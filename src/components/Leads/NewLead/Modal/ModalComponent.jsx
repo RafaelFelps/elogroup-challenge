5973c8f24e016f0c0f970e9c84f73142260aa223
Rafael Felilpe <rafaelfelps62@gmail.com>
2022-04-10 01:26:53 -0300

    Adicionando dependencias e babelconfig

--- src/components/Leads/NewLead/Modal/ModalComponent.jsx
+++ src/components/Leads/NewLead/Modal/ModalComponent.jsx
@@ -8,61 +8,6 @@ import Checkbox from "./CheckboxComponent"
 
 const Modal = ({ setIsOpen }) => {
 
-    const [isCheckAll, setIsCheckAll] = useState(false);
-    const [isCheck, setIsCheck] = useState([]);
-    const [list, setList] = useState([]);
-    useEffect(() => {
-        setList(Catalogues);
-    }, [list]);
-
-    const handleSelectAll = e => {
-        setIsCheckAll(!isCheckAll);
-        setIsCheck(list.map(li => li.id));
-        if (isCheckAll) {
-            setIsCheck([]);
-        }
-    };
-
-    const handleClick = e => {
-        const { id, checked } = e.target;
-        setIsCheck([...isCheck, id]);
-        if (!checked) {
-            setIsCheck(isCheck.filter(item => item !== id));
-        }
-    };
-
-    console.log(isCheck);
-
-    const catalog = list.map(({ id, name }) => {
-        return (
-            <>
-                <Checkbox
-                    key={id}
-                    type="checkbox"
-                    name={name}
-                    id={id}
-                    handleClick={handleClick}
-                    isChecked={isCheck.includes(id)}
-                />
-                {name}
-            </>
-        );
-    });
-
-    const seletcAllCheck = () => {
-        return (
-            <div>
-                <Checkbox
-                    type="checkbox"
-                    name="selectAll"
-                    id="selectAll"
-                    handleClick={handleSelectAll}
-                    isChecked={isCheckAll}
-                />
-            </div>
-        );
-    };
-
 
     return (
         <>
@@ -87,7 +32,6 @@ const Modal = ({ setIsOpen }) => {
                             <input placeholder="E-mail" name="email" type="text" />
                             <table name="leadsTable" className={styles.oportunityTable}>
                                 <thead>
-                                    {seletcAllCheck()}
                                 </thead>
                                 <tbody>
 
