5806b0b782c7f271cf34cf7facc56c4c39d16d52
Rafael Felilpe <rafaelfelps62@gmail.com>
2022-04-11 23:26:14 -0300

    Removing app

--- src/components/Leads/LeadsComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Leads from './LeadsComponent';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Leads />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/components/Register/RegisterComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RegisterComponent from './RegisterComponent';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<RegisterComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
